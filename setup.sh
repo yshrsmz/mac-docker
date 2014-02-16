abe8b101961085795ce3871221cfd418a0c995d2
yshrsmz <the.phantom.bane@gmail.com>
2014-02-17 00:20:58 +0900

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
 
-
+################################################################################
 # install boot2docker
-mkdir ./bin
-cd ./bin
+#mkdir ./bin
+#cd ./bin
 
 # Get boot2docker
-curl https://raw.github.com/steeve/boot2docker/master/boot2docker > boot2docker
-
+#curl https://raw.github.com/steeve/boot2docker/master/boot2docker > boot2docker
 
 # Mark it executable
-chmod +x boot2docker
+#chmod +x boot2docker
 
 # install docker client
-curl -o docker http://get.docker.io/builds/Darwin/x86_64/docker-latest
+#curl -o docker http://get.docker.io/builds/Darwin/x86_64/docker-latest
 
 # Mark it executable
-chmod +x docker
+#chmod +x docker
+
+# copy the executable file
+#sudo cp docker /usr/local/bin/
+################################################################################
+
+# install docker & boot2docker via Homebrew
+brew update
+brew install boot2docker
 
 # Set the environment variable for the docker daemon
 export DOCKER_HOST=tcp://
 
-# copy the executable file
-sudo cp docker /usr/local/bin/
+
